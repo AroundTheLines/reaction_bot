c7203bff83a3f01d0f899e402a74d6c34d597df8
Qile Wang <wangqile123@gmail.com>
2016-03-14 09:25:10 -0400

    it works again

--- routes/reactions.js
+++ routes/reactions.js
@@ -5,19 +5,14 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var router = express.Router();
 
-// Replace with your Slack API token
 var slackToken = process.env.SLACK_API_TOKEN || '';
 
 var rtm = new RtmClient(slackToken, {logLevel: 'debug'});
 rtm.start();
 
-// Name of the person to be reacted to
 var nameFor = 'michael';
-// Name of emoji to be reacted with
-var emoji = 'shio';
-
-// Don't touch this one
 var nameId = '';
+var emoji = 'shio';
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 	for (var j = 0; j < rtmStartData.users.length; j++){
@@ -32,11 +27,15 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   console.log("The message is: " + message.text);
   if(message.user === nameId){
-  	request.get('https://slack.com/api/reactions.add?token=' + slackToken + '&name=' + emoji +'&channel=' + message.channel + '&timestamp=' + message.ts)
+  	request.get('https://slack.com/api/reactions.add?token=' + slackToken + '&name=' + emoji + '&channel=' + message.channel + '&timestamp=' + message.ts)
   	.on('response', function(response){
   		console.log(response.headers);
   	});
   }
+  // request.get('https://slack.com/api/reactions.add?token=' + slackToken + '&name=shio&channel=' + message.channel + '&timestamp=' + message.ts)
+  // 	.on('response', function(response){
+  // 		console.log(response.headers);
+  // 	});
 });
 
-module.exports = router;
+module.exports = router; 
